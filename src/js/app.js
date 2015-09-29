2c912f340d7990bfd60bd11ab87c0463c8703fb7
thujeevan <thujee@gmail.com>
2015-09-29 10:58:13 +0530

    scaffolding structure

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+React.render(<h2>Clean slate</h2>, document.getElementById('main'));
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    entry: __dirname + '/src/js/app.js',
+    output: {
+        path: __dirname + '/dist/assets',
+        filename: 'bundle.js',
+        publicPath: '/assets/'
+    },
+    module: {
+        loaders: [{
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }, {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            }, {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!less-loader'
+            }, // use ! to chain loaders
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }, {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=8192'
+            } // inline base64 URLs for <=8k images, direct URLs for the rest
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+};
